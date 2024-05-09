9870fc808b444d64b98cc7a27417e941b1b5b927
Bryan McCarthy <bryanmccarthy2@gmail.com>
2024-05-09 22:04:20 +0100

    animating

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
+#include <string>
 
 #define START_POS_X 10
 #define START_POS_Y 50
@@ -11,6 +12,12 @@ int main(int argc, char* argv[])
     int numRows = 1;
     int numCols = 1;
     float scaleFactor = 1.0;
+    bool animationPlaying = false;
+    int currFrameX = 0;
+    int currFrameY = 0;
+
+    sf::Clock clock;
+    float frameDuration = 0.2f;
 
     sf::Texture texture;
     if(!texture.loadFromFile("player_run.png"))
@@ -20,26 +27,43 @@ int main(int argc, char* argv[])
 
     sf::Sprite sprite(texture);
     sprite.setPosition(START_POS_X, START_POS_Y);
+
+    sf::Sprite frameSprite(texture);
+    frameSprite.setPosition(10, 250);
     
-    int spriteSheetWidth = texture.getSize().x; // Width of the sprite sheet
-    int spriteSheetHeight = texture.getSize().y; // Height of the sprite sheet
+    int spriteSheetWidth = texture.getSize().x;
+    int spriteSheetHeight = texture.getSize().y;
     int cellWidth = spriteSheetWidth / numCols;
     int cellHeight = spriteSheetHeight / numRows;
 
-    // Define the border for the sprite sheet
     sf::RectangleShape border(sf::Vector2f(spriteSheetWidth, spriteSheetHeight));
     border.setPosition(START_POS_X, START_POS_Y);
     border.setFillColor(sf::Color::Transparent);
     border.setOutlineColor(sf::Color::White);
     border.setOutlineThickness(1);
 
-    // sf::Font font;
-    // if (!font.loadFromFile("Anonymous_Pro.ttf"))
-    //     return EXIT_FAILURE;
-    // sf::Text text("Hello SFML", font, 50);
+    sf::RectangleShape frameBorder(sf::Vector2f(spriteSheetWidth, spriteSheetHeight));
+    frameBorder.setPosition(10, 250);
+    frameBorder.setFillColor(sf::Color::Transparent);
+    frameBorder.setOutlineColor(sf::Color::White);
+    frameBorder.setOutlineThickness(1);
+    
+    sf::Font font;
+    if(!font.loadFromFile("Anonymous_Pro.ttf"))
+    {
+        return EXIT_FAILURE;
+    }
 
-    // Use something like this for when animating
-    // sprite.setTextureRect(sf::IntRect(0, 0, 50, 50)); // Set the rectangle to (0,0) with width 50 and height 50
+    sf::Text rowsDisplayString("ROWS:" + std::to_string(numRows), font, 24);
+    sf::Text colsDisplayString("COLS:" + std::to_string(numCols), font, 24);
+    rowsDisplayString.setPosition(5, 5);
+    colsDisplayString.setPosition(100, 5);
+
+    sf::Text scaleDisplayString("SCALE:" + std::to_string(scaleFactor), font, 24);
+    scaleDisplayString.setPosition(220, 5);
+
+    sf::Text frameSizeDisplayString("FRAME:" + std::to_string(cellWidth) + "x" + std::to_string(cellHeight), font, 24);
+    frameSizeDisplayString.setPosition(420, 5);
     
     while(window.isOpen())
     {
@@ -53,48 +77,64 @@ int main(int argc, char* argv[])
                     window.close();
                     break;
                 case sf::Event::KeyPressed:
-                    if(event.key.code == sf::Keyboard::Q)
+                    switch(event.key.code)
                     {
-                        numCols++;
-                        cellWidth = spriteSheetWidth / numCols;
-                    }
-                    else if(event.key.code == sf::Keyboard::A)
-                    {
-                        if(numCols > 1)
-                        {
-                            numCols--;
+                        case sf::Keyboard::Q:
+                            numCols++;
                             cellWidth = spriteSheetWidth / numCols;
-                        }
-                    }
-                    else if(event.key.code == sf::Keyboard::W)
-                    {
-                        numRows++;
-                        cellHeight = spriteSheetHeight / numRows;
-                    }
-                    else if(event.key.code == sf::Keyboard::S)
-                    {
-                        if(numRows > 1)
-                        {
-                            numRows--;
+                            colsDisplayString.setString("COLS:" + std::to_string(numCols));
+                            frameSizeDisplayString.setString("FRAME:" + std::to_string(cellWidth) + "x" + std::to_string(cellHeight));
+                            frameBorder.setSize(sf::Vector2f(cellWidth, cellHeight));
+                            frameSprite.setTextureRect(sf::IntRect(0, 0, cellWidth, cellHeight));
+                            break;
+                        case sf::Keyboard::A:
+                            if(numCols > 1)
+                            {
+                                numCols--;
+                                cellWidth = spriteSheetWidth / numCols;
+                                colsDisplayString.setString("COLS:" + std::to_string(numCols));
+                                frameSizeDisplayString.setString("FRAME:" + std::to_string(cellWidth) + "x" + std::to_string(cellHeight));
+                                frameBorder.setSize(sf::Vector2f(cellWidth, cellHeight));
+                                frameSprite.setTextureRect(sf::IntRect(0, 0, cellWidth, cellHeight));
+                            }
+                            break;
+                        case sf::Keyboard::W:
+                            numRows++;
                             cellHeight = spriteSheetHeight / numRows;
-                        }
-                    }
-                    else if(event.key.code == sf::Keyboard::Up)
-                    {
-                        scaleFactor = scaleFactor + 0.1;
-                        sprite.setScale(scaleFactor, scaleFactor);
-                        border.setScale(scaleFactor, scaleFactor);
-                        std::cout << "ScaleFactor: " << scaleFactor << std::endl;
-                    }
-                    else if(event.key.code == sf::Keyboard::Down)
-                    {
-                        if(scaleFactor > 0.6)
-                        {
-                            scaleFactor = scaleFactor - 0.1;
+                            rowsDisplayString.setString("ROWS:" + std::to_string(numRows));
+                            frameSizeDisplayString.setString("FRAME:" + std::to_string(cellWidth) + "x" + std::to_string(cellHeight));
+                            frameBorder.setSize(sf::Vector2f(cellWidth, cellHeight));
+                            frameSprite.setTextureRect(sf::IntRect(0, 0, cellWidth, cellHeight));
+                            break;
+                        case sf::Keyboard::S:
+                            if(numRows > 1)
+                            {
+                                numRows--;
+                                cellHeight = spriteSheetHeight / numRows;
+                                rowsDisplayString.setString("ROWS:" + std::to_string(numRows));
+                                frameSizeDisplayString.setString("FRAME:" + std::to_string(cellWidth) + "x" + std::to_string(cellHeight));
+                                frameBorder.setSize(sf::Vector2f(cellWidth, cellHeight));
+                                frameSprite.setTextureRect(sf::IntRect(0, 0, cellWidth, cellHeight));
+                            }
+                            break;
+                        case sf::Keyboard::Up:
+                            scaleFactor = scaleFactor + 0.1;
                             sprite.setScale(scaleFactor, scaleFactor);
                             border.setScale(scaleFactor, scaleFactor);
-                        }
-                        std::cout << "ScaleFactor: " << scaleFactor << std::endl;
+                            scaleDisplayString.setString("SCALE:" + std::to_string(scaleFactor));
+                            break;
+                        case sf::Keyboard::Down:
+                            if(scaleFactor > 0.6)
+                            {
+                                scaleFactor = scaleFactor - 0.1;
+                                sprite.setScale(scaleFactor, scaleFactor);
+                                border.setScale(scaleFactor, scaleFactor);
+                            scaleDisplayString.setString("SCALE:" + std::to_string(scaleFactor));
+                            }
+                        case sf::Keyboard::Z:
+                            animationPlaying = !animationPlaying;
+                        default:
+                            break;
                     }
                 default:
                     break;
@@ -105,10 +145,15 @@ int main(int argc, char* argv[])
 
         window.draw(sprite);
         window.draw(border);
+        window.draw(frameSprite);
+        window.draw(frameBorder);
 
-        // window.draw(text);
+        window.draw(rowsDisplayString);
+        window.draw(colsDisplayString);
+        window.draw(scaleDisplayString);
+        window.draw(frameSizeDisplayString);
 
-        // Draw vertical lines
+        // Draw column splits
         for (int col = 1; col < numCols; col++)
         {
             sf::Vertex line[] =
@@ -120,7 +165,7 @@ int main(int argc, char* argv[])
             window.draw(line, 2, sf::Lines);
         }
 
-        // Draw horizontal lines
+        // Draw row splits
         for (int row = 1; row < numRows; row++)
         {
             sf::Vertex line[] =
@@ -130,6 +175,24 @@ int main(int argc, char* argv[])
             };
 
             window.draw(line, 2, sf::Lines);
+        }
+
+        if (animationPlaying && clock.getElapsedTime().asSeconds() > frameDuration) {
+            // Move to the next frame
+            currFrameX++;
+            if (currFrameX >= numCols) {
+                currFrameX = 0;
+                currFrameY++;
+                if (currFrameY >= numRows) {
+                    currFrameY = 0; // Loop back to the first frame
+                }
+            }
+
+            // Set the new texture rectangle for the current frame
+            frameSprite.setTextureRect(sf::IntRect(currFrameX * cellWidth, currFrameY * cellHeight, cellWidth, cellHeight));
+
+            // Reset the clock for the next frame
+            clock.restart();
         }
 
         window.display();
