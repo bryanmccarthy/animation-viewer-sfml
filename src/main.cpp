9891df9bfd6274d3cbd908ee456a322756b7d25e
Bryan McCarthy <bryanmccarthy2@gmail.com>
2024-05-10 12:37:31 +0100

    gui wip

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
-#include <SFML/Graphics.hpp>
 #include <string>
+#include <SFML/Graphics.hpp>
+#include <SFML/Graphics/Color.hpp>
 
-#define START_POS_X 10
-#define START_POS_Y 50
+#define SPRITESHEET_POS_X 5
+#define SPRITESHEET_POS_Y 5
 
 int main(int argc, char* argv[])
 {
-    sf::RenderWindow window(sf::VideoMode(1000, 800), "animation viewer");
+    sf::RenderWindow window(sf::VideoMode(800, 600), "animation viewer");
 
     int numRows = 1;
     int numCols = 1;
@@ -25,27 +26,52 @@ int main(int argc, char* argv[])
         return EXIT_FAILURE;
     }
 
-    sf::Sprite sprite(texture);
-    sprite.setPosition(START_POS_X, START_POS_Y);
+    sf::Color gray(144, 144, 144, 255);
+    sf::Color brown_one(38, 18, 20, 255);
+    sf::Color brown_two(77, 44, 41, 255);
+    sf::Color brown_three(120, 67, 44, 255);
+    sf::Color brown_four(181, 117, 74, 255);
+    sf::Color brown_five(209, 163, 138, 255);
+
+    sf::Sprite spritesheet(texture);
+    spritesheet.setPosition(SPRITESHEET_POS_X, SPRITESHEET_POS_Y);
 
     sf::Sprite frameSprite(texture);
-    frameSprite.setPosition(10, 250);
+    frameSprite.setPosition(10, 360);
     
     int spriteSheetWidth = texture.getSize().x;
     int spriteSheetHeight = texture.getSize().y;
     int cellWidth = spriteSheetWidth / numCols;
     int cellHeight = spriteSheetHeight / numRows;
 
-    sf::RectangleShape border(sf::Vector2f(spriteSheetWidth, spriteSheetHeight));
-    border.setPosition(START_POS_X, START_POS_Y);
-    border.setFillColor(sf::Color::Transparent);
-    border.setOutlineColor(sf::Color::White);
-    border.setOutlineThickness(1);
+    sf::RectangleShape spritesheetBackground(sf::Vector2f(650, 350));
+    spritesheetBackground.setPosition(0, 0);
+    spritesheetBackground.setFillColor(brown_one);
+    spritesheetBackground.setOutlineColor(sf::Color::Black);
+    spritesheetBackground.setOutlineThickness(3);
+
+    sf::RectangleShape frameSpriteBackground(sf::Vector2f(650, 230));
+    frameSpriteBackground.setPosition(0, 350);
+    frameSpriteBackground.setFillColor(brown_one);
+    frameSpriteBackground.setOutlineColor(sf::Color::Black);
+    frameSpriteBackground.setOutlineThickness(3);
+
+    sf::RectangleShape configBackground(sf::Vector2f(150, 600));
+    configBackground.setPosition(650, 0);
+    configBackground.setFillColor(brown_two);
+    configBackground.setOutlineColor(sf::Color::Black);
+    configBackground.setOutlineThickness(3);
+
+    sf::RectangleShape spritesheetBorder(sf::Vector2f(spriteSheetWidth, spriteSheetHeight));
+    spritesheetBorder.setPosition(SPRITESHEET_POS_X, SPRITESHEET_POS_Y);
+    spritesheetBorder.setFillColor(sf::Color::Transparent);
+    spritesheetBorder.setOutlineColor(gray);
+    spritesheetBorder.setOutlineThickness(1);
 
     sf::RectangleShape frameBorder(sf::Vector2f(spriteSheetWidth, spriteSheetHeight));
-    frameBorder.setPosition(10, 250);
+    frameBorder.setPosition(10, 360);
     frameBorder.setFillColor(sf::Color::Transparent);
-    frameBorder.setOutlineColor(sf::Color::White);
+    frameBorder.setOutlineColor(gray);
     frameBorder.setOutlineThickness(1);
     
     sf::Font font;
@@ -55,15 +81,13 @@ int main(int argc, char* argv[])
     }
 
     sf::Text rowsDisplayString("ROWS:" + std::to_string(numRows), font, 24);
+    rowsDisplayString.setPosition(660, 10);
     sf::Text colsDisplayString("COLS:" + std::to_string(numCols), font, 24);
-    rowsDisplayString.setPosition(5, 5);
-    colsDisplayString.setPosition(100, 5);
-
-    sf::Text scaleDisplayString("SCALE:" + std::to_string(scaleFactor), font, 24);
-    scaleDisplayString.setPosition(220, 5);
-
-    sf::Text frameSizeDisplayString("FRAME:" + std::to_string(cellWidth) + "x" + std::to_string(cellHeight), font, 24);
-    frameSizeDisplayString.setPosition(420, 5);
+    colsDisplayString.setPosition(660, 50);
+    sf::Text frameSizeDisplayString("FRAME:" + std::to_string(cellWidth) + "x" + std::to_string(cellHeight), font, 16);
+    frameSizeDisplayString.setPosition(5, 580);
+    sf::Text scaleDisplayString("SCALE:" + std::to_string(scaleFactor), font, 16);
+    scaleDisplayString.setPosition(125, 580);
     
     while(window.isOpen())
     {
@@ -119,17 +143,17 @@ int main(int argc, char* argv[])
                             break;
                         case sf::Keyboard::Up:
                             scaleFactor = scaleFactor + 0.1;
-                            sprite.setScale(scaleFactor, scaleFactor);
-                            border.setScale(scaleFactor, scaleFactor);
+                            spritesheet.setScale(scaleFactor, scaleFactor);
+                            spritesheetBorder.setScale(scaleFactor, scaleFactor);
                             scaleDisplayString.setString("SCALE:" + std::to_string(scaleFactor));
                             break;
                         case sf::Keyboard::Down:
                             if(scaleFactor > 0.6)
                             {
                                 scaleFactor = scaleFactor - 0.1;
-                                sprite.setScale(scaleFactor, scaleFactor);
-                                border.setScale(scaleFactor, scaleFactor);
-                            scaleDisplayString.setString("SCALE:" + std::to_string(scaleFactor));
+                                spritesheet.setScale(scaleFactor, scaleFactor);
+                                spritesheetBorder.setScale(scaleFactor, scaleFactor);
+                                scaleDisplayString.setString("SCALE:" + std::to_string(scaleFactor));
                             }
                         case sf::Keyboard::Z:
                             animationPlaying = !animationPlaying;
@@ -143,23 +167,21 @@ int main(int argc, char* argv[])
 
         window.clear();
 
-        window.draw(sprite);
-        window.draw(border);
+        window.draw(spritesheetBackground);
+        window.draw(frameSpriteBackground);
+        
+        window.draw(spritesheet);
+        window.draw(spritesheetBorder);
         window.draw(frameSprite);
         window.draw(frameBorder);
-
-        window.draw(rowsDisplayString);
-        window.draw(colsDisplayString);
-        window.draw(scaleDisplayString);
-        window.draw(frameSizeDisplayString);
 
         // Draw column splits
         for (int col = 1; col < numCols; col++)
         {
             sf::Vertex line[] =
             {
-                sf::Vertex(sf::Vector2f(START_POS_X + col * (cellWidth * scaleFactor), START_POS_Y)),
-                sf::Vertex(sf::Vector2f(START_POS_X + col * (cellWidth * scaleFactor), START_POS_Y + (spriteSheetHeight * scaleFactor)))
+                sf::Vertex(sf::Vector2f(SPRITESHEET_POS_X + col * (cellWidth * scaleFactor), SPRITESHEET_POS_Y), gray),
+                sf::Vertex(sf::Vector2f(SPRITESHEET_POS_X + col * (cellWidth * scaleFactor), SPRITESHEET_POS_Y + (spriteSheetHeight * scaleFactor)), gray)
             };
 
             window.draw(line, 2, sf::Lines);
@@ -170,8 +192,8 @@ int main(int argc, char* argv[])
         {
             sf::Vertex line[] =
             {
-                sf::Vertex(sf::Vector2f(START_POS_X, START_POS_Y + row * (cellHeight * scaleFactor))),
-                sf::Vertex(sf::Vector2f(START_POS_X + (spriteSheetWidth * scaleFactor), START_POS_Y + row * (cellHeight * scaleFactor)))
+                sf::Vertex(sf::Vector2f(SPRITESHEET_POS_X, SPRITESHEET_POS_Y + row * (cellHeight * scaleFactor)), gray),
+                sf::Vertex(sf::Vector2f(SPRITESHEET_POS_X + (spriteSheetWidth * scaleFactor), SPRITESHEET_POS_Y + row * (cellHeight * scaleFactor)), gray)
             };
 
             window.draw(line, 2, sf::Lines);
@@ -194,6 +216,12 @@ int main(int argc, char* argv[])
             // Reset the clock for the next frame
             clock.restart();
         }
+
+        window.draw(configBackground);
+        window.draw(rowsDisplayString);
+        window.draw(colsDisplayString);
+        window.draw(scaleDisplayString);
+        window.draw(frameSizeDisplayString);
 
         window.display();
     }
